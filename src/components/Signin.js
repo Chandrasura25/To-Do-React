9b9d4fcbaf96bbc14c26ac9298ec0f71c18cf349
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2022-06-30 16:43:25 +0100

    update

--- src/components/Signin.js
+++ src/components/Signin.js
@@ -31,7 +31,7 @@ export default function Signin() {
                 navigate('/todo')
             }
             else{
-                alert("incorrect username or password");
+                // alert("incorrect username or password");
                 // window.location.reload();
             }
         } 
